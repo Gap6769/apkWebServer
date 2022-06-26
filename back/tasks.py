a85e525c504c5b30dc1a4265a8f217bfaa91bc37
Gap6769 <david.aviles.brun@gmail.com>
2022-06-26 16:37:57 -0400

    docker and socket implement

--- back/tasks.py
+++ back/tasks.py
@@ -1,7 +1,11 @@
 from back.models import Messages, dump_sms
 import socketserver
+
 from celery import shared_task
-import ast
+
+import json
+import re
+import datetime
 
 
 @shared_task
@@ -25,20 +29,18 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
         content = self.request.recv(1024).decode()
         ip = self.client_address[0]
-
-        """$#$  _id:1 thread_id:3 address:45645645f person:null date:1656175264721 date_sent:0 protocol:null read:1 status:-1 type:2 reply_path_present:null subject:null body:Hola service_center:null locked:0 sub_id:1 error_code:-1 creator:com.google.android.apps.messaging seen:1"""
         if "$#$" == content[0:3]:
-            content = ast.literal_eval(content[3:])
+            content = json.loads(content.replace("$#$ ", "").replace("'", '"').replace("\n", " "))
             dump_sms.objects.create(
                 sms_type=content["type"],
                 phone_number=content["address"],
-                date=content["date"],
+                date=datetime.datetime.fromtimestamp(int(content["date"]) / 1000.0).strftime("%Y-%m-%d %H:%M:%S"),
                 status=content["status"],
                 body=content["body"],
             )
-            print(content)
+            print(content["body"])
         else:
-            Messages.objects.create(ip=self.client_address[0], message=self.request.recv(1024).decode())
+            # Messages.objects.create(ip=self.client_address[0], message=self.request.recv(1024).decode())
             print("New Message from {}".format(self.client_address[0]))
             print("Message: {}".format(self.request.recv(1024)))
             # just send back the same data, but upper-cased
