27aef1cf0d1f71eb40035171d60c4399ffe1882b
Gap6769 <david.aviles.brun@gmail.com>
2022-06-26 20:38:00 -0400

    ok

--- z3api/urls.py
+++ z3api/urls.py
@@ -17,10 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.response import Response
+from rest_framework import status, permissions
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('back.urls')),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include("back.urls"))]
 
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
